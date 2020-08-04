a619fcb920c32689d27be0156d2d01e47c1d350d
Dirk van Goethem <vangoethem.dirk@hotmail.com>
2020-08-04 11:20:50 +0200

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-
 require 'credentials.php';
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+// include all your model files here
+require 'model/Customer.php';
+//include all your controllers here
+require 'Controller/HomePageController.php;
