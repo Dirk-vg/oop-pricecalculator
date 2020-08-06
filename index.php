e93ee3deb00503a64cf70d2dbb0d56d5bbec2978
Dirk van Goethem <vangoethem.dirk@hotmail.com>
2020-08-06 10:56:47 +0200

    refactor of index.php

--- index.php
+++ index.php
@@ -1,41 +1,20 @@
 <?php
-
-require 'Model/Databasemanager.php';
-require 'Model/Product.php';
-require 'View/Viewproduct.php';
-
-
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
+declare(strict_types=1);
+ini_set('display_errors', "1");
+ini_set('display_startup_errors', "1");
 error_reporting(E_ALL);
 
-// include all your model files here
-require 'Model/Customer.php';
-//include all your controllers here
-require 'Controller/HomePageController.php';
+require_once 'credentials.php';
 
-$controller = new HomePageController();
-$controller->render($_GET, $_POST);
+# INCLUDE MODEL FILES
+require_once 'Model/Customer.php';
+require_once 'Model/Customer.php';
+require_once 'Model/Discount.php';
+require_once 'Model/Product.php';
+require_once 'Model/Login.php';
+require_once 'Model/DatabaseManager.php';
 
-$products = new Viewproduct();
-$products->showAllProcucts();
+# INCLUDE CONTROLLERS HERE
+require_once 'Controller/HomePageController.php';
 
-/*$dbhost = "localhost";
-$dbuser = "becode";
-$dbpass = "becode123";
-$dbname = "pricecalculator";
-
-    // set DSN
-    $dsn = 'mysql:host=' . $dbhost . ';dbname=' . $dbname;
-
-    // create a PDO instance
-    $pdo = new PDO($dsn, $dbuser, $dbpass);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-
-
-    //PDO QUERY
-    $stmt = $pdo->query('SELECT * FROM product ');
-    while ($row = $stmt ->fetch(PDO::FETCH_ASSOC)) {
-        echo $row['name']. '<br>';
-}
-*/
+require_once 'View/homepage.php';
